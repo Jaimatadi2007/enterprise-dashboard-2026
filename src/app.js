6ad7e1b900c8062a95ed7993aa0d573896192bb6
Jaimatadi2007 <himanshu.hs.sharma@gmail.com>
2026-02-24 00:15:16 +0530

    Added modular health route

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const healthRoutes = require("./routes/health.routes");
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api", healthRoutes);
 
 app.get("/", (req, res) => {
   res.json({ message: "Enterprise Architect API is running 🚀" });
